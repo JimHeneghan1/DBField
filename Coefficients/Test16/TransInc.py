71a86f3bd62ac0667c53c0b2cf026167438d347c
Jim <dermoth@uga.edu>
2017-10-10 00:58:01 -0400

    removed the text files

--- Coefficients/Test16/TransInc.py
+++
@@ -1,9 +0,0 @@
-#libraries *******************************************************************
-from numpy import *
-from scipy import *
-from pylab import *
-
-Ez = loadtxt('Ez900.txt', usecols=(1,), skiprows=1, unpack = True)
-
-plot(Ez)
-show()
